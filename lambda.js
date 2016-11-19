f00b692555a014b188e6de55b56dcb438ad7c62a
Krish Dholakiya <krishna.dholakiya@gmail.com>
2016-11-19 08:51:17 -0700

    Migrated to ES6

--- lambda.js
+++ lambda.js
@@ -1,6 +1,9 @@
+'use strict';
+
 process.env.NODE_CONFIG_DIR = process.env["LAMBDA_TASK_ROOT"];
-var request = require('request');
-var cheerio = require('cheerio');
+
+let request = require('request');
+let cheerio = require('cheerio');
 
 
 module.exports = {
@@ -8,20 +11,19 @@ module.exports = {
     request.get('https://api.genius.com/search?q=' 
       + event.songName + 
       '&access_token=' + process.env.ACCESS_TOKEN, 
-    function(err, res, body) {
-      desiredSongURL = JSON.parse(body).response.hits.filter(function(a) {
-        if (a.result.stats.pageviews) {
+    (err, res, body) => {
+      let desiredSongURL = JSON.parse(body)
+        .response.hits.filter(a => {
+          if (a.result.stats.pageviews) {
             return true;
-        }
-        return false;
-      })[0].result.url;
-
-      request.get(desiredSongURL, function(errS, resS, bodyS) {
-        var $ = cheerio.load(bodyS);
+          }
+          return false;
+        })[0].result.url;
+      
+      request.get(desiredSongURL, (errS, resS, bodyS) => {
+        let $ = cheerio.load(bodyS);
         callback(null, $('.lyrics').text())
       });
-
-      ;
     });
   }
 }
